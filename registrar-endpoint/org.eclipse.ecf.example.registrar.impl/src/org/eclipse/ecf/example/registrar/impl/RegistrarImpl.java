5ceb868ce829725545e7fe9ac79ddcae0b5a0b26
slewis <slewis@composent.com>
2019-10-14 21:18:25 -0700

    Added copyright notice for source files.

--- registrar-endpoint/org.eclipse.ecf.example.registrar.impl/src/org/eclipse/ecf/example/registrar/impl/RegistrarImpl.java
+++ registrar-endpoint/org.eclipse.ecf.example.registrar.impl/src/org/eclipse/ecf/example/registrar/impl/RegistrarImpl.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Composent, Inc, Ryan Moquin, and Elsivier Inc. All rights 
+ * reserved. This  program and the accompanying materials are made available 
+ * under the terms of the Apache  Public License v2.0 which accompanies this 
+ * distribution, and is available at https://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Contributors: Ryan Moquin and Scott Lewis - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.ecf.example.registrar.impl;
 
 import java.util.concurrent.ConcurrentHashMap;
